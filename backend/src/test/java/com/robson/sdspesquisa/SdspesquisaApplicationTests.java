8373a22b97c48417db92205276f433fb6f0591cf
Robson <robsong200@gmail.com>
2020-09-14 21:37:12 -0300

    Primeira classe de entidade

---
+++ backend/src/test/java/com/robson/sdspesquisa/SdspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robson.sdspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
