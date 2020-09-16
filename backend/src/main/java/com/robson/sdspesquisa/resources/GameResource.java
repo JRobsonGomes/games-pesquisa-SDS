4862619824dd20c4131bb4df76540ab6c9bd5f0b
Robson <robsong200@gmail.com>
2020-09-15 21:50:50 -0300

    Criando a camada de servico e GameDTO

--- backend/src/main/java/com/robson/sdspesquisa/resources/GameResource.java
+++ backend/src/main/java/com/robson/sdspesquisa/resources/GameResource.java
@@ -4,21 +4,23 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.robson.sdspesquisa.entities.Game;
-import com.robson.sdspesquisa.repositories.GameRepository;
+import com.robson.sdspesquisa.dto.GameDTO;
+import com.robson.sdspesquisa.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
 public class GameResource {
 
 	@Autowired
-	private GameRepository gameRepository;
+	private GameService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Game>> findAll(){
-		List<Game> list = gameRepository.findAll();
+	public ResponseEntity<List<GameDTO>> findAll(){
+		List<GameDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ backend/src/main/java/com/robson/sdspesquisa/services/GameService.java
@@ -0,0 +1,25 @@
+package com.robson.sdspesquisa.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.robson.sdspesquisa.dto.GameDTO;
+import com.robson.sdspesquisa.entities.Game;
+import com.robson.sdspesquisa.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll(){
+		List<Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+}
