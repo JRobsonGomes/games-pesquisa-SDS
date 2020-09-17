71387e60b61f460acedd686d2c8cc8b2ec769085
Robson <robsong200@gmail.com>
2020-09-16 21:41:08 -0300

    Producao1

--- backend/src/main/java/com/robson/sdspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/robson/sdspesquisa/repositories/RecordRepository.java
@@ -3,7 +3,6 @@ package com.robson.sdspesquisa.repositories;
 import java.time.Instant;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
